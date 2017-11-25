c8045ff9e92f210b4ecce6228cde499434787026
Minh <trinhvanminh2009@gmail.com>
2017-11-25 19:45:55 +0700

    Update
    Update

--- EDU_SUPPORT_SOFT/app/src/main/java/com/example/pc/edu_support_soft/MainActivity.java
+++ EDU_SUPPORT_SOFT/app/src/main/java/com/example/pc/edu_support_soft/MainActivity.java
@@ -3,17 +3,23 @@ package com.example.pc.edu_support_soft;
 import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.ColorFilter;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -26,6 +32,7 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.xw.repo.BubbleSeekBar;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -47,12 +54,19 @@ public class MainActivity extends AppCompatActivity {
     private Uri imageUri;
     private static int PICTURE_RESULT =1;
     private int countLab = 0;
+    private SeekBar seekBar;
+    private BubbleSeekBar bubbleSeekBar;
+    private static final int REQUEST_PERMISSION = 101;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSION);
        /* getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
        if(getSupportActionBar() != null){
@@ -65,9 +79,26 @@ public class MainActivity extends AppCompatActivity {
         bitmap = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher);
         btnShow = (CircleButton) findViewById(R.id.btnShow);
         editTextInfo = (EditText) findViewById(R.id.editTextInfo);
+        seekBar = (SeekBar)findViewById(R.id.seekBar);
+        
         databaseReference = FirebaseDatabase.getInstance().getReference();
         mStorageRef = FirebaseStorage.getInstance().getReferenceFromUrl("gs://edusupportsoft.appspot.com");
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                configBrightness(imageView);
+            }
 
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
 
         ///take pic
@@ -189,6 +220,30 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_PERMISSION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1]==PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Log.e("Error","Permission not allow");
+                } else {
+                    System.exit(1);
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
     private void updateCountLab(){
         databaseReference.child("Lab1").addValueEventListener(new ValueEventListener() {
                                 @Override
@@ -207,5 +262,20 @@ public class MainActivity extends AppCompatActivity {
         Matrix matrix = new Matrix();
         matrix.postRotate((float) 90);
         return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
+
+    private void configBrightness(ImageView iv){
+
+        float brightness = (float)(seekBar.getProgress() - 255);
+
+        float[] colorMatrix = {
+                1,0,0,0,brightness,
+                0,1f,0,0,brightness,
+                0,0,1f,0,brightness,
+                0,0,0,1f,0
+        };
+
+        ColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
+        iv.setColorFilter(colorFilter);
     }
 }
