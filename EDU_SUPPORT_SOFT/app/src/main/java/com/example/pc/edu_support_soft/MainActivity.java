922962be07bb4cfcb7b33d262a312a16b8944f97
Minh <trinhvanminh2009@gmail.com>
2017-11-26 09:46:44 +0700

    Last commit
    Last commit

--- EDU_SUPPORT_SOFT/app/src/main/java/com/example/pc/edu_support_soft/MainActivity.java
+++ EDU_SUPPORT_SOFT/app/src/main/java/com/example/pc/edu_support_soft/MainActivity.java
@@ -14,15 +14,22 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -34,8 +41,14 @@ import com.google.firebase.storage.UploadTask;
 import com.xw.repo.BubbleSeekBar;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import at.markushi.ui.CircleButton;
 import cn.pedant.SweetAlert.SweetAlertDialog;
@@ -55,6 +68,8 @@ public class MainActivity extends AppCompatActivity {
     private int countLab = 0;
     private BubbleSeekBar bubbleSeekBar;
     private static final int REQUEST_PERMISSION = 101;
+    private TextView detectedView;
+    private static final String TAG = "Error";
 
 
 
@@ -77,6 +92,7 @@ public class MainActivity extends AppCompatActivity {
         bitmap = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher);
         btnShow = (CircleButton) findViewById(R.id.btnShow);
         editTextInfo = (EditText) findViewById(R.id.editTextInfo);
+        detectedView = (TextView)findViewById(R.id.detectedView);
         databaseReference = FirebaseDatabase.getInstance().getReference();
         mStorageRef = FirebaseStorage.getInstance().getReferenceFromUrl("gs://edusupportsoft.appspot.com");
         bubbleSeekBar = (BubbleSeekBar)findViewById(R.id.bubbleSeekBar);
@@ -159,9 +175,16 @@ public class MainActivity extends AppCompatActivity {
                         Solution solution;
                         if(downloadUrl[0] != null){
                             if(editTextInfo.getText().toString().trim().equals("")){
-                                solution = new Solution(editTextInfo.getText().toString().equals("")? "Unknown":
-                                        editTextInfo.getText().toString(),
-                                        downloadUrl[0].toString(),0);
+                                if(detectedView.getText() != null && detectedView.getText().toString().trim().equals("")){
+                                    solution = new Solution(editTextInfo.getText().toString().equals("")? "Unknown":
+                                            editTextInfo.getText().toString(),
+                                            downloadUrl[0].toString(),0);
+                                }else{
+                                    solution = new Solution(editTextInfo.getText().toString().equals("")? "Unknown":
+                                            editTextInfo.getText().toString(),
+                                            downloadUrl[0].toString(),0);
+                                }
+
                             }else {
                                 solution = new Solution(editTextInfo.getText().toString().equals("")? editTextInfo.getText().toString().trim():
                                         editTextInfo.getText().toString(),
@@ -210,6 +233,9 @@ public class MainActivity extends AppCompatActivity {
             if(requestCode == PICTURE_RESULT && resultCode == Activity.RESULT_OK){
                 bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
                 imageView.setImageBitmap(bitmap);
+               /* if (imageUri != null) {
+                    inspect(imageUri);
+                }*/
             }
         }catch (NullPointerException e){
             Toast.makeText(this, "You haven't take a picture yet !", Toast.LENGTH_SHORT).show();
@@ -276,4 +302,76 @@ public class MainActivity extends AppCompatActivity {
         ColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
         iv.setColorFilter(colorFilter);
     }
+
+    private void inspectFromBitmap(Bitmap bitmap) {
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(this).build();
+        try {
+            if (!textRecognizer.isOperational()) {
+                new AlertDialog.
+                        Builder(this).
+                        setMessage("Text recognizer could not be set up on your device").show();
+                return;
+            }
+
+            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+            SparseArray<TextBlock> origTextBlocks = textRecognizer.detect(frame);
+            List<TextBlock> textBlocks = new ArrayList<>();
+            for (int i = 0; i < origTextBlocks.size(); i++) {
+                TextBlock textBlock = origTextBlocks.valueAt(i);
+                textBlocks.add(textBlock);
+            }
+            Collections.sort(textBlocks, new Comparator<TextBlock>() {
+                @Override
+                public int compare(TextBlock o1, TextBlock o2) {
+                    int diffOfTops = o1.getBoundingBox().top - o2.getBoundingBox().top;
+                    int diffOfLefts = o1.getBoundingBox().left - o2.getBoundingBox().left;
+                    if (diffOfTops != 0) {
+                        return diffOfTops;
+                    }
+                    return diffOfLefts;
+                }
+            });
+
+            StringBuilder detectedText = new StringBuilder();
+            for (TextBlock textBlock : textBlocks) {
+                if (textBlock != null && textBlock.getValue() != null) {
+                    detectedText.append(textBlock.getValue());
+                    detectedText.append("\n");
+                }
+            }
+
+            detectedView.setText(detectedText);
+        }
+        finally {
+            textRecognizer.release();
+        }
+    }
+
+    private void inspect(Uri uri) {
+        InputStream is = null;
+        Bitmap bitmap = null;
+        try {
+            is = getContentResolver().openInputStream(uri);
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            options.inSampleSize = 2;
+            options.inScreenDensity = DisplayMetrics.DENSITY_LOW;
+            bitmap = BitmapFactory.decodeStream(is, null, options);
+            inspectFromBitmap(bitmap);
+        } catch (FileNotFoundException e) {
+            Log.w(TAG, "Failed to find the file: " + uri, e);
+        } finally {
+            if (bitmap != null) {
+                bitmap.recycle();
+            }
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    Log.w(TAG, "Failed to close InputStream", e);
+                }
+            }
+        }
+    }
+
 }
--- EDU_SUPPORT_SOFT/app/src/main/java/com/example/pc/edu_support_soft/Solution.java
+++ EDU_SUPPORT_SOFT/app/src/main/java/com/example/pc/edu_support_soft/Solution.java
@@ -8,12 +8,20 @@ public class Solution {
     private String whoDidIt;
     private String bitmapUrl;
     private int score;
+    private String textExtraction;
 
 
     public Solution(String whoDidIt, String bitmapUrl, int score) {
         this.whoDidIt = whoDidIt;
         this.bitmapUrl = bitmapUrl;
         this.score = score;
+    }
+
+    public Solution(String whoDidIt, String bitmapUrl, int score, String textExtraction) {
+        this.whoDidIt = whoDidIt;
+        this.bitmapUrl = bitmapUrl;
+        this.score = score;
+        this.textExtraction = textExtraction;
     }
 
     public String getWhoDidIt() {
